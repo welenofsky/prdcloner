d76d1c84a77cec9566f09eb2a1587dac864da533
Justin Welenofsky <welenofsky@gmail.com>
2023-03-03 15:00:17 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='prdcloner',
       version='0.1',
       description='Clones Blogvault site to server',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/welenofsky/prdcloner',
       author='Justin Welenofsky',
       author_email='welenofsky@gmail.com',
       license='MIT',
